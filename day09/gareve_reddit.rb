a03ee8e3703f0c9a38c44c79627fe88062349467
Brett Rann <brettrann@gmail.com>
2015-12-23 20:03:26 +1100

    gareve's elegant ruby

---
+++ day09/gareve_reddit.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+dist = {}
+File.readlines('input.txt').map(&:split).each do |x, to, y, equals, d|
+  dist[[x,y].sort] = d.to_i
+end
+
+p dist.keys.flatten.uniq.permutation.map { |comb|
+  comb.each_cons(2).reduce(0) {|s, x| s+ dist[x.sort] }
+}.sort.rotate(-1).first(2)
